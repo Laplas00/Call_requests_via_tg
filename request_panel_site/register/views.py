179796810c48e5d90ab827db0e11dfb0671d9b71
Bogdan <61288992+Laplas00@users.noreply.github.com>
2024-10-21 15:49:49 +0300

    new

--- request_panel_site/register/views.py
+++ request_panel_site/register/views.py
@@ -7,12 +7,27 @@ import json
 
 
 def login_page(request):
-    print('login page loaing..')
+    print('lodaing login page')
     content = render(request, 'login_page.html').content
     response = HttpResponse(content)
     response.headers['Content-Security-Policy'] = "default-src 'self' https://core.telegram.org https://oauth.telegram.org; frame-ancestors 'self' https://core.telegram.org https://oauth.telegram.org"
     response.headers['X-Frame-Options'] = 'ALLOW-FROM https://core.telegram.org https://oauth.telegram.org'
     return response
 
+
+@require_http_methods(['POST'])
+def login_view(request):
+    try:
+        user_data = json.loads(request.body)
+        print(user_data)  # Process the user's data here
+        print('hello world!!@#!@#!@#')
+        return JsonResponse({'message': 'Login successful'})
+    except Exception as e:
+        print(f"Error: {e}")
+        return JsonResponse({'message': 'Error occurred'})
+
+
 def login_authentication(request):
     ...
+
+
