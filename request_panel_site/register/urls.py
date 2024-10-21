b7fb2bcf5893a83df60c6c6b6ab10fa239ecdfc6
Bogdan <61288992+Laplas00@users.noreply.github.com>
2024-10-21 16:36:51 +0300

    refresh

--- request_panel_site/register/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("login/", views.login_page, name='login_page'),
-    path("telegram-login/", views.login_view, name='login_view'),
-]
