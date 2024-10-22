21c8e55880e8a57f8ec6951b99e15861a2dadb03
Bogdan <61288992+Laplas00@users.noreply.github.com>
2024-10-22 17:39:46 +0300

    n

--- request_panel_site/request_panel_site/settings.py
+++ request_panel_site/request_panel_site/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-&(9v#1oxj(knjv0*fd^ih__t(*$*#z785alv=l_e-ycfjr_7c('
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['116.203.69.73', 'localhost',
                  'salesarm.multywhale.pro', 'www.salesarm.multywhale.pro']
